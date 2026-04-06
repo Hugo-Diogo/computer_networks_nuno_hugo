b0a667cfaf344843cc045ee6bfb34da2679d1f69
Hugo Diogo <hugomsdiogo@gmail.com>
2026-04-06 15:22:59 +0100

    final

--- write_noncanonical.c
+++ write_noncanonical.c
@@ -3,6 +3,7 @@
 //  Modified by: Eduardo Nuno Almeida [enalmeida@fe.up.pt]
 #define _POSIX_SOURCE 1 // POSIX compliant source
 
+#include "link_layer.h"
 #include <fcntl.h>
 #include <signal.h>
 #include <stdio.h>
@@ -13,20 +14,12 @@
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
-#include "link_layer.h"
-
 
 extern volatile int STOP;
 extern int alarmEnabled;
 extern int alarmCount;
 
 unsigned char buf_temp = {0};
-
-
-
-
-
-
 
 //--------------------------------------------------------------------main
 int main(int argc, char *argv[]) {
@@ -41,27 +34,21 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  char* path = choose_file();
-    FILE* file = open_file(path);
+  char *path = choose_file();
+  FILE *file = open_file(path);
 
+  // tamanho
+  fseek(file, 0, SEEK_END);
+  long filesize = ftell(file);
+  fseek(file, 0, SEEK_SET);
 
-    // tamanho
-    fseek(file, 0, SEEK_END);
-    long filesize = ftell(file);
-    fseek(file, 0, SEEK_SET);
+  // nome do ficheiro
+  char *filename = strrchr(path, '/');
 
-    // nome do ficheiro
-    char* filename = strrchr(path, '/');
-
-    if (filename != NULL)
-        filename++;
-    else
-        filename = path;
-
-
-
-
-
+  if (filename != NULL)
+    filename++;
+  else
+    filename = path;
 
   // Program usage: Uses either COM1 or COM2
   const char *serialPortName = argv[1];
@@ -77,7 +64,6 @@ int main(int argc, char *argv[]) {
   // Open serial port device for reading and writing, and not as controlling tty
   // because we don't want to get killed if linenoise sends CTRL-C.
   int fd = llopen(serialPortName);
-
 
   if (fd < 0) {
     perror(serialPortName);
@@ -122,83 +108,72 @@ int main(int argc, char *argv[]) {
   }
   unsigned char frame_begin[5];
 
-    int s_begin = send_CONN(frame_begin);
-        if (!send_with_retry(fd, frame_begin, s_begin)) {
-        close(fd);
-        return -1;
-    }
-
-
+  int s_begin = send_CONN(frame_begin);
+  if (!send_with_retry(fd, frame_begin, s_begin)) {
+    close(fd);
+    return -1;
+  }
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////. start packet. ////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////. start
+  ///packet.///////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // Create string to send
   unsigned char frame[BUF_SIZE];
 
+  unsigned char start_packet[512];
+  memset(start_packet, 0, 512);
+  int start_size = build_start_packet(start_packet, filesize, filename);
 
+  int size = build_frame(frame, start_packet, start_size, 0);
+  print_hex(start_packet, start_size);
 
+  if (!send_with_retry(fd, frame, size)) {
+    close(fd);
+    return -1;
+  }
 
-    unsigned char start_packet[512];
-    memset(start_packet, 0, 512);
-    int start_size = build_start_packet(start_packet, filesize, filename);
-
-    int size = build_frame(frame, start_packet, start_size, 0);
-    print_hex(start_packet, start_size);
-
-    if (!send_with_retry(fd, frame, size)) {
-        close(fd);
-        return -1;
-    }
-
-
-
-  
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////. data packet. ////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int bytes = 0;
-unsigned char buf_file[BUF_file_SIZE]= {0};
-unsigned char buf_application[BUF_file_SIZE] = {0};
-int j = 1;
-while((bytes = fread(buf_file, 1, BUF_file_SIZE - 3, file)) != 0){
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////. data
+  ///packet.///////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  int bytes = 0;
+  unsigned char buf_file[BUF_file_SIZE] = {0};
+  unsigned char buf_application[BUF_file_SIZE] = {0};
+  int j = 1;
+  while ((bytes = fread(buf_file, 1, BUF_file_SIZE - 3, file)) != 0) {
 
     buf_application[0] = 1;
     buf_application[1] = (bytes >> 8) & 0xFF;
     buf_application[2] = bytes & 0xFF;
 
-
     memcpy(&buf_application[3], buf_file, bytes);
 
-size = build_frame(frame, buf_application, bytes +3, j);
-print_hex(frame, size);
-j++;
-        if (!send_with_retry(fd, frame, size)) {
-            close(fd);
-            return -1;
-        }
+    size = build_frame(frame, buf_application, bytes + 3, j);
+    print_hex(frame, size);
+    j++;
+    if (!send_with_retry(fd, frame, size)) {
+      close(fd);
+      return -1;
     }
+  }
+  printf("last packet sent.\n");
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////. end
+  ///packet.///////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////. end packet. ////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    unsigned char end_packet[512];
-    memset(end_packet, 0, 512);
-    int end_size = build_end_packet(end_packet, filesize, filename);
+  unsigned char end_packet[512];
+  memset(end_packet, 0, 512);
+  int end_size = build_end_packet(end_packet, filesize, filename);
   size = build_frame(frame, end_packet, end_size, j);
 
   send_with_retry(fd, frame, size);
 
   print_hex(frame, end_size);
 
+  tcsetattr(fd, TCSANOW, &oldtio);
+  close(fd);
 
-
-    tcsetattr(fd, TCSANOW, &oldtio);
-    close(fd);
-
-    return 0;
+  return 0;
 }
