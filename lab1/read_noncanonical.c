6dc1f2f212ab3e9c8ce9d121123f5af0bea9bef5
ninth-n <njc.loureiro@sapo.pt>
2026-04-21 15:03:00 +0100

    move lab1 files

---
+++ lab1/read_noncanonical.c
@@ -0,0 +1,333 @@
+// Read from serial port in non-canonical mode
+//
+// Modified by: Eduardo Nuno Almeida [enalmeida@fe.up.pt]
+
+
+
+/*
+Things to change:
+
+For the application layer we need to send a START packet with name and size of the file. THIS IS A CONTROL PACKET.    
+The we need to divide the file into smaller parts and include a header in each one;
+Then we send that DATA packet.
+
+After sending the last data packet we need to announce to the recveiver that the transfer is finalised
+->Send another CONTROL packet indicating that the transfer has ended 
+
+*/
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+#include "link_layer.h"
+
+
+// Baudrate settings are defined in <asm/termbits.h>, which is
+// included by <termios.h>
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define FALSE 0
+#define TRUE 1
+
+#define BUF_SIZE 1024
+
+FILE *f_file;
+enum state {start, FLAG_RCV, A_RCV, C_RCV, information, BCC_OK, stop};
+
+char xor(unsigned char array[], int cont){
+    char test = 0x00;
+    for(int i = 4; i <= cont; i++){
+        test = test ^ array[i];    
+    }
+    return test;
+}
+
+int main(int argc, char *argv[])
+{   
+
+
+
+    enum state st = start;
+
+    // Program usage: Uses either COM1 or COM2
+    const char *serialPortName = argv[1];
+
+    if (argc < 2)
+    {
+        printf("Incorrect program usage\n"
+               "Usage: %s <SerialPort>\n"
+               "Example: %s /dev/ttyS1\n",
+               argv[0],
+               argv[0]);
+        exit(1);
+    }
+
+    // Open serial port device for reading and writing and not as controlling tty
+    // because we don't want to get killed if linenoise sends CTRL-C.
+    int fd = open(serialPortName, O_RDWR | O_NOCTTY);
+    if (fd < 0)
+    {
+        perror(serialPortName);
+        exit(-1);
+    }
+
+    struct termios oldtio;
+    struct termios newtio;
+
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0; // Inter-character timer unused
+    newtio.c_cc[VMIN] = 1;  // Blocking read until 5 chars received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+    // Loop for input
+    unsigned char buf[BUF_SIZE];
+    unsigned char cur;
+    unsigned char destuffed[BUF_file_SIZE];
+    int i = 0;
+    int j = 0;
+    unsigned char a_rcv; 
+    unsigned char c_rcv;
+    unsigned char t_bcc2 = 0x00;
+    int b = 0;
+    while(b==0)
+    {
+        int res = read(fd, &cur, 1);
+        printf("Received byte: %02X\n", cur);
+
+
+        //printf("State: %d\n",st);
+
+        if (res == 0) continue;
+
+        switch(st){
+            case start:
+                if (cur == 0x7E) st = FLAG_RCV;
+                break;
+
+            case FLAG_RCV:
+                if (cur == 0x03){
+                    a_rcv = cur;
+                    t_bcc2 = 0x00;
+                    st = A_RCV;
+                } else if (cur == A_RX){
+                    a_rcv = A_RX;
+                    st = A_RCV;
+                }
+                else if (cur == 0x7E) st = FLAG_RCV;
+                break;
+            
+            case A_RCV:
+                if (cur == 0x7E) st = FLAG_RCV;
+                else{
+                    c_rcv = cur;
+                    st = C_RCV;
+                }
+                break;
+            
+            case C_RCV:
+                if (cur == (a_rcv ^ c_rcv)) st = BCC_OK;
+                else if (cur == 0x7E) st = FLAG_RCV;
+                break;
+            
+            case BCC_OK:
+                if (cur == 0x7E) {
+                    if (c_rcv == 0x03) {
+
+                        send_RR(fd, 0);
+                        st = FLAG_RCV;
+                    } else 
+                    {
+                        send_REJ(fd, j);
+                        st = FLAG_RCV;
+                        }
+                }else if (c_rcv == 0x00 || c_rcv == 0x40){
+                            buf[i++] = cur;
+                            t_bcc2 ^= cur;
+                            st = information;
+
+                }
+                
+                break;
+
+case information:
+
+    if (cur == 0x7E) {
+
+
+
+
+        // DESTUFF PRIMEIRO (IMPORTANTE)
+        long size = distuffing(buf, i, destuffed);
+        printf("%02X\n", destuffed[0]);
+        //print_hex(buf, i);
+        if (destuffed[0] == 0x03) {
+                    printf("END packet\n");
+                    send_DISC(fd);
+                    handle_end_packet(f_file);
+                    b = 1;
+                    st = stop;
+                    break;
+        }
+
+        // SEPARAR BCC2 (já destuffed)
+        unsigned char received_bcc2 = destuffed[size - 1];
+        // CALCULAR BCC2
+        unsigned char calc_bcc2 = 0x00;
+        long size_app = 0;
+        for (int k = 0; k < size - 1; k++) {
+            calc_bcc2 ^= destuffed[k];
+        }
+
+        printf("BCC2 Tx: %d\nBCC2 Rx: %d\n", received_bcc2, calc_bcc2);
+
+
+        if (calc_bcc2 == received_bcc2) {
+
+            print_hex(destuffed, size);
+
+            // sequência correta
+            if ((c_rcv == 0x00 && j == 0) || (c_rcv == 0x40 && j == 1)) {
+
+                // APPLICATION LAYER
+                if (destuffed[0] == 0x02) {
+                    printf("START packet\n");
+                    f_file = handle_start_packet(destuffed, size);
+                }
+                else if (destuffed[0] == 0x01) {
+                    printf("DATA packet\n");
+                    if (f_file)
+                        handle_data_packet(destuffed, f_file);
+                }
+
+                // RR
+                if (j == 0) {
+                    printf("OK! RR1\n");
+                    send_RR(fd, 1);
+                    j = 1;
+
+
+
+                } else {
+                    printf("OK! RR0\n");
+                    send_RR(fd, 0);
+                    j = 0;
+
+
+
+                }
+
+            } else {
+                // duplicada
+                if (j == 0) {
+                    printf("RR1 DUP\n"); 
+                    send_RR(fd, 1);
+                                        i = 0;
+                    st = start;
+                }else {
+                    printf("RR0 DUP\n"); 
+                    send_RR(fd, 0);
+                                        i = 0;
+                    st = start;
+                }
+            }
+
+        } else {
+            // erro BCC2
+            if (c_rcv == 0x00){
+                    printf("ERRO! 0\n"); 
+                    send_REJ(fd, 0);
+                    i = 0;
+                    st = start;
+                    break;}
+            else{
+                    printf("ERRO! 1\n"); 
+                    send_REJ(fd, 1);
+                                        i = 0;
+                    st = start;
+                    break;}
+        }
+
+        // reset
+        i = 0;
+        st = FLAG_RCV;
+        break;
+    }
+
+
+
+    else {
+        if (i < BUF_SIZE) {
+            buf[i++] = cur;
+            break;
+        }
+    }
+
+    
+
+    case stop:
+        printf("STOP\n");
+        send_DISC(fd);
+        handle_end_packet(f_file);
+        b = 1;
+        break;
+    }
+}
+
+
+
+
+        
+        
+        
+    // The while() cycle should be changed in order to respect the specifications
+    // of the protocol indicated in the Lab guide
+
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    close(fd);
+
+    return 0;
+}
---
+++ lab1/write_noncanonical.c
@@ -0,0 +1,208 @@
+// Wite to serial port in non-canonical mode
+//
+//  Modified by: Eduardo Nuno Almeida [enalmeida@fe.up.pt]
+#define _POSIX_SOURCE 1 // POSIX compliant source
+#define _POSIX_C_SOURCE 199309L
+#include "link_layer.h"
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <time.h>
+#include <unistd.h>
+
+extern volatile int STOP;
+extern int alarmEnabled;
+extern int alarmCount;
+
+unsigned char buf_temp = {0};
+
+//--------------------------------------------------------------------main
+int main(int argc, char *argv[]) {
+
+  // Set alarm function handler.
+  // Install the function signal to be automatically invoked when the timer
+  // expires, invoking in its turn the user function alarmHandler
+  struct sigaction act = {0};
+  act.sa_handler = &alarmHandler;
+  if (sigaction(SIGALRM, &act, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
+
+  char *path = choose_file();
+  FILE *file = open_file(path);
+
+  // tamanho
+  fseek(file, 0, SEEK_END);
+  long filesize = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  // nome do ficheiro
+  char *filename = strrchr(path, '/');
+
+  if (filename != NULL)
+    filename++;
+  else
+    filename = path;
+
+  // Program usage: Uses either COM1 or COM2
+  const char *serialPortName = argv[1];
+
+  if (argc < 2) {
+    printf("Incorrect program usage\n"
+           "Usage: %s <SerialPort>\n"
+           "Example: %s /dev/ttyS1\n",
+           argv[0], argv[0]);
+    exit(1);
+  }
+
+  // Open serial port device for reading and writing, and not as controlling tty
+  // because we don't want to get killed if linenoise sends CTRL-C.
+  int fd = llopen(serialPortName);
+
+  if (fd < 0) {
+    perror(serialPortName);
+    exit(-1);
+  }
+
+  struct termios oldtio;
+  struct termios newtio;
+
+  // Save current port settings
+  if (tcgetattr(fd, &oldtio) == -1) {
+    perror("tcgetattr");
+    exit(-1);
+  }
+
+  // Clear struct for new port settings
+  memset(&newtio, 0, sizeof(newtio));
+
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
+
+  // Set input mode (non-canonical, no echo,...)
+  newtio.c_lflag = 0;
+  newtio.c_cc[VTIME] = 0; // Inter-character timer unused
+  newtio.c_cc[VMIN] = 5;  // Blocking read until 5 chars received
+
+  // VTIME e VMIN should be changed in order to protect with a
+  // timeout the reception of the following character(s)
+
+  // Now clean the line and activate the settings for the port
+  // tcflush() discards data written to the object referred to
+  // by fd but not transmitted, or data received but not read,
+  // depending on the value of queue_selector:
+  //   TCIFLUSH - flushes data received but not read.
+  tcflush(fd, TCIOFLUSH);
+
+  // Set new port settings
+  if (tcsetattr(fd, TCSANOW, &newtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
+  unsigned char frame_begin[5];
+
+  int s_begin = send_CONN(frame_begin);
+  if (!send_with_retry(fd, frame_begin, s_begin)) {
+    close(fd);
+    return -1;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////. start
+  /// packet.///////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+    struct timespec start, end;
+    
+    
+    // Create string to send
+  unsigned char frame[BUF_SIZE];
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+  unsigned char start_packet[512];
+  memset(start_packet, 0, 512);
+  int start_size = build_start_packet(start_packet, filesize, filename);
+
+  int size = build_frame(frame, start_packet, start_size, 0);
+  print_hex(start_packet, start_size);
+
+  if (!send_with_retry(fd, frame, size)) {
+    close(fd);
+    return -1;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////. data
+  /// packet.///////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  int bytes = 0;
+  unsigned char buf_file[BUF_file_SIZE] = {0};
+  unsigned char buf_application[BUF_file_SIZE] = {0};
+  int j = 1;
+  while ((bytes = fread(buf_file, 1, BUF_file_SIZE - 3, file)) != 0) {
+
+    buf_application[0] = 1;
+    buf_application[1] = (bytes >> 8) & 0xFF;
+    buf_application[2] = bytes & 0xFF;
+
+    memcpy(&buf_application[3], buf_file, bytes);
+
+    size = build_frame(frame, buf_application, bytes + 3, j);
+    print_hex(frame, size);
+    j++;
+    int res_send = send_with_retry(fd, frame, size);
+
+    }
+
+  
+  printf("last packet sent.\n");
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////. end
+  /// packet.///////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+   clock_gettime(CLOCK_MONOTONIC, &end);
+    
+    
+
+  unsigned char end_packet[512];
+  memset(end_packet, 0, 512);
+  int end_size = build_end_packet(end_packet, filesize, filename);
+  size = build_frame(frame, end_packet, end_size, j);
+
+  send_with_retry(fd, frame, size);
+
+  print_hex(frame, end_size);
+
+double time = 
+            (end.tv_sec - start.tv_sec) + 
+            (end.tv_nsec - start.tv_nsec) / 1e9;
+
+    printf("Transfer time: %f seconds.\n", time);
+    
+    double R = (filesize * 8) / time;
+
+    printf("Rb (Bitrate): %f bps\n", R);
+
+    double C = 38400;
+    double S = R / C;
+
+    printf("Efficiency S = %f\n", S);
+
+
+
+  tcsetattr(fd, TCSANOW, &oldtio);
+  close(fd);
+
+  return 0;
+}
